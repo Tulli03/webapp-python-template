57163fd866b5dab0843e5235de39ea0f91c198ef
Marconi <marconi@marconicivitavecchia.it>
2019-12-03 10:35:00 +0100

    gg

---
+++ film.py
@@ -0,0 +1,12 @@
+class Film:
+    def __init__(title, year, producer, duration, actors, resolution):
+        self.title = title
+        self.year = year
+        self.producer = producer
+        self.duration = duration
+        self.actors = actors
+        self.resolution = resolution
+
+
+
+    self.actors[]
